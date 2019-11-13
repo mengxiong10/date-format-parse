5dc3e3e13d76bdcca48f170332cff4909d6af1fb
mengxiong10 <15623530290@163.com>
2019-11-13 16:00:47 +0800

    style: prettier

--- build/locale.js
+++ build/locale.js
@@ -5,32 +5,50 @@ const libLocales = path.resolve(__dirname, '../node_modules/moment/locale');
 
 const localLocalePath = path.resolve(__dirname, '../src/locale');
 
-function generateLocales() {
-  fs.readdirSync(libLocales).forEach((name, i) => {
-    const key = name.replace(/\.js$/, '');
-    const lang = moment.localeData(key);
-    const result = {
-      months: lang.months(),
-      monthsShort: lang.monthsShort(),
-      weekdays: lang.weekdays(),
-      weekdaysShort: lang.weekdaysShort(),
-      weekdaysMin: lang.weekdaysMin(),
-      firstDayOfWeek: lang._week.dow || 0,
-      firstWeekContainsDate: lang._week.doy || 1,
-    };
-    const template = `
+function writeFile(key, data) {
+  fs.writeFile(path.join(localLocalePath, `${key}.ts`), data, err => {
+    if (err) {
+      throw err;
+    }
+  });
+}
+
+function getTemplate(key) {
+  const lang = moment.localeData(key);
+  const result = {
+    months: lang.months(),
+    monthsShort: lang.monthsShort(),
+    weekdays: lang.weekdays(),
+    weekdaysShort: lang.weekdaysShort(),
+    weekdaysMin: lang.weekdaysMin(),
+    firstDayOfWeek: lang._week.dow || 0,
+    firstWeekContainsDate: lang._week.doy || 1,
+  };
+  const template = `
 import { Locale } from '../locale';
 
 const locale: Locale = ${JSON.stringify(result)}
 
 export default locale;
-    `;
-    const data = template.trim();
-    fs.writeFile(path.join(localLocalePath, `_${key}.ts`), data, err => {
-      if (err) {
-        throw err;
-      }
-    });
+  `;
+  return template.trim();
+}
+
+function test() {
+  fs.readdirSync(localLocalePath).forEach(name => {
+    if (!/\.ts$/.test(name)) {
+      return;
+    }
+    const key = name.replace(/\.ts$/, '');
+    const data = getTemplate(key);
+    writeFile(key, data);
+  });
+}
+
+function generateLocales() {
+  fs.readdirSync(libLocales).forEach((name, i) => {
+    const key = name.replace(/\.js$/, '');
+    const data = getTemplate(key);
   });
 
   // Object.keys(libLocales).forEach(locale => {
@@ -46,3 +64,4 @@ export default locale;
 }
 
 generateLocales();
+// test();
