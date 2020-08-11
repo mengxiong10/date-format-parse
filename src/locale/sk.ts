0a766336ac2224ccb4bdd2303becbabdbb79c993
VeeeneX <VeeeneX@users.noreply.github.com>
2020-08-12 00:24:03 +0200

    Slovak translations
    Added missing slovak translation

---
+++ src/locale/sk.ts
@@ -0,0 +1,13 @@
+import { Locale } from '../locale';
+
+const locale: Locale = {
+  months: ['Január', 'Február', 'Marec', 'Apríl', 'Máj', 'Jún', 'Júl', 'August', 'September', 'Október', 'November', 'December'],
+  monthsShort: ['Jan', 'Feb', 'Mar', 'Apr', 'Máj', 'Jún', 'Júl', 'Aug', 'Sep', 'Okt', 'Nov', 'Dec'],
+  weekdays: ['Nedeľa', 'Pondelok', 'Utorok', 'Streda', 'Štvrtok', 'Piatok', 'Sobota'],
+  weekdaysShort: ['Ned', 'Pon', 'Uto', 'Str', 'Štv', 'Pia', 'Sob'],
+  weekdaysMin: ['Ne', 'Po', 'Ut', 'St', 'Št', 'Pia', 'So'],
+  firstDayOfWeek: 1,
+  firstWeekContainsDate: 1
+};
+
+export default locale;
